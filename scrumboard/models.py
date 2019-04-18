afb318d95720594112c390bb8b8c86c0b9e7e975
DKelly <kelly13493@gmail.com>
2019-04-17 21:43:50 -0400

    Add project data model

--- scrumboard/models.py
+++ scrumboard/models.py
@@ -1,13 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    members = models.ManyToManyField(User, related_name='projects')
+
+    def __str__(self):
+        return "Project: {}".format(self.name)
+
+        
 class List(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return "List: {}".format(self.name)
-
 
 
 class Card(models.Model):
